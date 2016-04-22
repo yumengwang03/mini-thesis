bae62a2c4984ebf1104c1e26dd71f1564aa21a72
yumengwang03 <wangy594@newschool.edu>
2016-04-22 19:38:44 -0400

    hidden-number

--- hidden-number/sketch.js
+++ hidden-number/sketch.js
@@ -2,10 +2,10 @@ var startB;
 var started;
 var rocks = [];
 var words = [];
+var posList = [];
+var hiddenPos = [];
 var moveStarted;
 var pitSize;
-
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,7 +18,16 @@ function setup() {
     started = true;
   });
 
-  pitSize = height / 2;
+  pitSize = height / 2 + 60;
+
+  var sevenSize = pitSize / 6;
+  var seven0 = createVector(width / 2 - 1.2 * sevenSize, height / 2 - sevenSize);
+  var seven1 = createVector(width / 2 - 0.2 * sevenSize, height / 2 - sevenSize);
+  var seven2 = createVector(width / 2 + 0.8 * sevenSize, height / 2 - sevenSize);
+  var seven3 = createVector(width / 2 + 0.6 * sevenSize, height / 2);
+  var seven4 = createVector(width / 2 + 0.4 * sevenSize, height / 2 + sevenSize);
+  var seven5 = createVector(width / 2 + 0.2 * sevenSize, height / 2 + 2 * sevenSize);
+  hiddenPos = [seven0, seven1, seven2, seven3, seven4, seven5];
 }
 
 function changeMoved1() {
@@ -31,44 +40,57 @@ function changeMoved2() {
 
 
 function draw() {
+  ellipse(width / 2, height / 2, pitSize, pitSize);
   if (started) {
     var r = 4;
     var c = 5;
     for (var i = 0; i < words.length; i++) {
       var unitX = width / c;
       var unitY = height / r;
+
       // sometimes I am good at math
-      var numPos = createVector(((i + c) % c) * unitX + random(0, unitX - 120), ((i + r) % r) * unitY + random(0, unitY - 120)); //to draw them in a grid with some randomness
+      // set rock positions in a grid with some randomness to distribute them evenly in the beginning
+      var numPos = createVector(((i + c) % c) * unitX + random(0, unitX - 150), ((i + r) % r) * unitY + random(0, unitY - 150));
+
+      // do not draw in the circle
       var safeDist = dist(numPos.x, numPos.y, width / 2, height / 2);
       if (safeDist >= pitSize / 2 + 80) {
-        rocks.push(new Rock(numPos.x, numPos.y, words[i]));
+        posList.push(numPos);
       }
+    }
+
+    for (var j = 0; j < posList.length; j++) {
+      rocks.push(new Rock(posList[j].x, posList[j].y, words[j]));
     }
     started = false;
     moveStarted = true;
   }
 
-  //console.log(rocks);
   if (moveStarted) {
-    for (var i = 0; i < words.length; i++) {
+    for (var i = 0; i < posList.length; i++) {
       rocks[i].update();
+      if (hiddenPos.length > 0) {
+        rocks[i].fall(hiddenPos[0].x, hiddenPos[0].y);
+      }
     }
   }
-
-  ellipse(width / 2, height / 2, pitSize, pitSize);
 }
 
 
 function Rock(posX, posY, word) {
   this.pos = createVector(0, 0);
-  this.size = createVector(120, 120);
+  this.size = createVector(150, 150);
   this.pos.set(posX, posY);
   this.word = word;
   this.wordRock = createButton(this.word);
-  this.wordRock.style('background', 'url(img/Logo.jpg)');
   this.wordRock.size(this.size.x, this.size.y);
   this.wordRock.moved = false;
   this.wordRock.position(this.pos.x, this.pos.y);
+  this.rockImg = ['url(img/rock.png)'];
+  this.runTime = 0;
+
+  this.wordRock.style('background', this.rockImg[0]);
+  this.wordRock.style('background-size', '100%');
 
   this.wordRock.mousePressed(changeMoved1);
   this.wordRock.mouseReleased(changeMoved2);
@@ -80,4 +102,20 @@ function Rock(posX, posY, word) {
       this.wordRock.position(this.pos.x, this.pos.y);
     }
   };
+
+  this.fall = function(hidPosX, hidPosY) {
+    // for (var i = 0; i < hiddenPos.length; i++) {
+    //   ellipse(hiddenPos[i].x, hiddenPos[i].y, 20, 20);
+    // }
+    this.newSafeDist = dist(this.pos.x, this.pos.y, width / 2, height / 2);
+    if (this.newSafeDist <= pitSize / 2 && !this.wordRock.moved && this.runTime < 1) {
+      this.wordRock.size(this.size.x / 3, this.size.y / 3);
+      this.pos.x = hidPosX;
+      this.pos.y = hidPosY;
+      this.wordRock.position(this.pos.x - 25, this.pos.y - 25);
+      hiddenPos.splice(0, 1);
+      this.wordRock.html('');
+      this.runTime++;
+    }
+  }
 }
