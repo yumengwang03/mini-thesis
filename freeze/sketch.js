a2f395cf3998e23317d81f0050ff44db0561fb4b
yumengwang03 <wangy594@newschool.edu>
2016-04-24 15:57:08 -0400

    progress

--- freeze/sketch.js
+++ freeze/sketch.js
@@ -5,8 +5,10 @@ var lightBList = []; //buttons
 var lights = [];
 //var lightOn;
 var lightIndex;
+var lightOrder;
 var lightTime;
 var lightPos;
+var textOut = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,6 +16,7 @@ function setup() {
   lightStarted = false;
   lightOn = false;
   lightIndex = 0;
+  lightOrder = 0;
   lightTime = 0;
   lightPos = createVector(0, 0);
 }
@@ -23,18 +26,20 @@ function draw() {
     for (var i = 0; i < 3; i++) {
       var lightB = createButton('test1');
       lightB.position(width / 3 * i + width / 6, height / 3);
+      lightB.id = i;
       lightB.mousePressed(lightClicked);
       lightBList.push(lightB);
-      var lightIn = createElement('textarea', 'hey1');
+      var lightIn = createElement('textarea', 'hey' + i);
       lightIn.position(width / 3 * i + width / 6, height / 3 + 20);
       lightQList.push(lightIn);
     }
     for (var i = 0; i < 2; i++) {
       var lightB = createButton('test2');
       lightB.position(width / 3 * i + 2 * width / 6, 2 * height / 3);
+      lightB.id = i + 3;
       lightB.mousePressed(lightClicked);
       lightBList.push(lightB);
-      var lightIn = createElement('textarea', 'hey2');
+      var lightIn = createElement('textarea', 'hey' + (i + 3));
       lightIn.position(width / 3 * i + 2 * width / 6, 2 * height / 3 + 20);
       lightQList.push(lightIn);
     }
@@ -44,7 +49,7 @@ function draw() {
   }
   if (lightStarted) {
     if (lightOn) {
-      if (millis() > lightTime + 100) {
+      if (millis() > lightTime + 120) {
         addLight();
         lightTime = millis();
       }
@@ -54,7 +59,7 @@ function draw() {
         lights[i].display();
         //console.log(lights.length);
 
-        if (lights.length > 10) {
+        if (lights.length >= textOut.length) {
           lights[0].photon.remove();
           lights.splice(0, 1);
 
@@ -67,39 +72,60 @@ function draw() {
 }
 
 function addLight() {
-  var textOut = ["pleasing", "complementary", "transparent", "keyboard", "banana", "texture", "tangible", "blue", "knitting", "pencil", "innovative", "carbon"];
-  if (lights.length <= 10 && lightPos.x != 0 && lightPos.y != 0) {
+  //textOut = ["pleasing", "complementary", "transparent", "keyboard", "banana", "texture", "tangible", "blue", "knitting", "pencil", "innovative", "carbon"];
+  console.log(textOut);
+  if (textOut.length < 10) {
+    textOut.push(textOut[lightOrder]);
+    lightOrder++;
+  }
+  if (textOut.length >= 10 && lights.length < textOut.length && lightPos.x != 0 && lightPos.y != 0) {
     var newLight = new Light(lightPos.x, lightPos.y, textOut[lightIndex]);
     lights.push(newLight);
     lightIndex++;
-    if (lightIndex > textOut.length) {
+    if (lightIndex >= textOut.length) {
       lightIndex = 0;
     }
   }
 }
 
 function lightClicked() {
-  //console.log(this.x, this.y);
   lightPos.set(this.x, this.y);
   lightOn = true;
+  var getText = lightQList[this.id].value();
+  textOut = getText.split(/\W+/);
+  for (var i = 0; i < textOut.length; i++) {
+    if(textOut[i] == "") {
+      textOut.splice(i, 1);
+    }
+  }
 }
 
 function Light(posX, posY, word) {
   this.pos = createVector(0, 0);
   this.pos.set(posX, posY);
-  //this.size = 
-  this.photon = createP(word);
+  this.size = 10;
+  this.s = 0;
+  this.fontS = 0.5;
+  this.photon = createButton(word);
   this.vel = 2;
 
-  this.update = function() {
-    this.pos.y += this.vel;
 
+  this.update = function() {
+    this.size = map(this.s, 0, 90, 10, 40);
+    this.fontS = map(this.s, 0, 90, 0.5, 2);
+    this.pos.y += this.vel;
+    this.s++;
   };
 
   this.display = function() {
-    this.photon.style('font-family', 'Avenir');
-    this.photon.style('font-weight', 'bold');
-    this.photon.position(this.pos.x, this.pos.y);
+    this.photon.size(5 * this.size, this.size);
+    this.photon.style('color', 'orange');
+    this.photon.style('font-size', this.fontS + 'em');
+    this.photon.style('font-family', 'monospace');
+    this.photon.style('outline', 'none');
+    this.photon.style('background-color', 'Transparent');
+    this.photon.style('border', 'white');
+    this.photon.position(this.pos.x - 5 * this.size / 2, this.pos.y);
 
   };
 }
