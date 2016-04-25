6401374188ac742c2f3e44a63532781359ad08a0
yumengwang03 <wangy594@newschool.edu>
2016-04-25 00:16:48 -0400

    more

--- freeze/sketch.js
+++ freeze/sketch.js
@@ -35,7 +35,7 @@ function draw() {
     road.position(0, 0);
     road.size(width, height);
     var endRoad = createElement('h1', 'Mysterious force is at the end of the road');
-    endRoad.position(width / 2 - 300, height / 2);
+    endRoad.position(width / 2 - 300, 7 * height / 8);
     for (var i = 0; i < 3; i++) {
       var lightB2 = createButton('off');
       lightB2.position(width / 3 * i + width / 6.2, height / 3 - areaSize / 2.8);
@@ -54,7 +54,7 @@ function draw() {
       lightBList.push(lightB);
 
       var lightIn = createElement('textarea', '');
-      lightIn.position(width / 3 * i + width / 6, height / 3 + 20);
+      lightIn.position(width / 3 * i + width / 5.5, height / 1.8);
       lightQList.push(lightIn);
     }
     for (var i = 0; i < 2; i++) {
@@ -75,7 +75,7 @@ function draw() {
       lightBList.push(lightB);
 
       var lightIn = createElement('textarea', '');
-      lightIn.position(width / 3 * i + 2 * width / 6, 2 * height / 3 + 20);
+      lightIn.position(width / 3 * i + 2 * width / 5.5, height / 1.8);
       lightQList.push(lightIn);
     }
 
@@ -98,6 +98,15 @@ function draw() {
       lightBList2[m].style('outline', 'none');
       lightBList2[m].style('border', 'white');
       lightBList2[m].style('color', 'white');
+
+      lightQList[m].size(120, 30);
+      lightQList[m].style('background', 'transparent');
+      lightQList[m].style('resize', 'none');
+      lightQList[m].style('border', 'dashed grey 2px');
+      lightQList[m].style('font-family', 'monospace');
+      lightQList[m].style('font-size', '1em');
+      lightQList[m].style('color', '#1BA200');
+      lightQList[m].style('outline', 'none');
     }
   }
 
@@ -145,9 +154,8 @@ function lightClicked() {
   var getText = lightQList[this.id].value();
 
   if (getText != '') {
-    //this.style('background', '#f2bf27');
-
     //this.class('lightButton');
+    this.html(' on');
 
     lightBList2[this.id].class('lightButton');
     this.clicked = true;
