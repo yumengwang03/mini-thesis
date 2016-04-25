df60280edb2a71b46d8e32c5284dd78d9e470e05
yumengwang03 <wangy594@newschool.edu>
2016-04-24 23:48:33 -0400

    more

--- freeze/sketch.js
+++ freeze/sketch.js
@@ -2,6 +2,7 @@ var lightRunOnce;
 var lightStarted;
 var lightQList = []; //textareas
 var lightBList = []; //buttons
+var lightBList2 = []; //real buttons
 var lightAreas = [];
 var lights = [];
 var lightOn;
@@ -30,36 +31,49 @@ function setup() {
 function draw() {
   if (lightRunOnce < 1) {
     background(0);
+    var road = createImg('img/road.png');
+    road.position(0, 0);
+    road.size(width, height);
+    var endRoad = createElement('h1', 'Mysterious force is at the end of the road');
+    endRoad.position(width / 2 - 300, height / 2);
     for (var i = 0; i < 3; i++) {
-      // var area = createImg('img/test.png');
-      // area.position(width / 3 * i + width / 6 - areaSize / 2, height / 3 - areaSize / 2);
-      // area.size(areaSize, areaSize);
-      // area.style('opacity', '0.2');
-      // lightAreas.push(area);
-      var lightB = createButton('test1');
-      lightB.position(width / 3 * i + width / 6, height / 3);
+      var lightB2 = createButton('off');
+      lightB2.position(width / 3 * i + width / 6.2, height / 3 - areaSize / 2.8);
+      lightBList2.push(lightB2);
+
+      var area = createImg('img/lampPost2.png');
+      area.position(width / 3 * i + width / 6 - areaSize / 3, height / 3 - areaSize / 1.6);
+      area.size(areaSize / 2.5, areaSize);
+
+      var lightB = createButton('off');
+      lightB.position(width / 3 * i + width / 6.5, height / 3 - areaSize / 2.2);
       lightB.id = i;
       lightB.clicked = false;
       lightB.mousePressed(lightClicked);
-      //lightB.class('lightButton');
+      // //lightB.class('lampLight');
       lightBList.push(lightB);
+
       var lightIn = createElement('textarea', '');
       lightIn.position(width / 3 * i + width / 6, height / 3 + 20);
       lightQList.push(lightIn);
     }
     for (var i = 0; i < 2; i++) {
-      // var area = createImg('img/test.png');
-      // area.position(width / 3 * i + 2 * width / 6 - areaSize / 2, 2 * height / 3 - areaSize / 2);
-      // area.size(areaSize, areaSize);
-      // area.style('opacity', '0.2');
-      // lightAreas.push(area);
-      var lightB = createButton('test2');
-      lightB.position(width / 3 * i + 2 * width / 6, 2 * height / 3);
+      var lightB2 = createButton('off');
+      lightB2.position(width / 3 * i + 2 * width / 6.1, 2 * height / 3 - areaSize / 6.8);
+      lightBList2.push(lightB2);
+
+      var area = createImg('img/lampPost2.png');
+      area.position(width / 3 * i + 2 * width / 6 - areaSize / 3, 2 * height / 3 - areaSize / 2.6);
+      area.size(areaSize / 2.5, areaSize);
+
+      var lightB = createButton('off');
+      lightB.position(width / 3 * i + 2 * width / 6.25, 2 * height / 3 - areaSize / 4.7);
       lightB.id = i + 3;
       lightB.clicked = false;
       lightB.mousePressed(lightClicked);
-      //lightB.class('lightButton');
+      //lightB.class('lampLight');
       lightBList.push(lightB);
+
       var lightIn = createElement('textarea', '');
       lightIn.position(width / 3 * i + 2 * width / 6, 2 * height / 3 + 20);
       lightQList.push(lightIn);
@@ -67,6 +81,24 @@ function draw() {
 
     lightRunOnce++;
     lightStarted = true;
+
+    for (var m = 0; m < lightBList.length; m++) {
+      lightBList[m].size(areaSize / 8, areaSize / 8);
+      lightBList[m].style('background', 'transparent');
+      lightBList[m].style('background-size', '100%');
+      lightBList[m].style('outline', 'none');
+      lightBList[m].style('font-family', 'monospace');
+      lightBList[m].style('font-size', areaSize / 220 + 'em');
+      lightBList[m].style('border', 'white');
+      lightBList[m].style('color', 'white');
+      lightBList[m].style('border-radius', '15px');
+
+      lightBList2[m].size(2, 2);
+      lightBList2[m].style('background', 'transparent');
+      lightBList2[m].style('outline', 'none');
+      lightBList2[m].style('border', 'white');
+      lightBList2[m].style('color', 'white');
+    }
   }
 
   if (lightStarted && lightOn) {
@@ -84,14 +116,14 @@ function draw() {
         lights.splice(0, 1);
       }
     }
-    
+
     var clickArray = [lightBList[0].clicked, lightBList[1].clicked, lightBList[2].clicked, lightBList[3].clicked, lightBList[4].clicked];
-    var allClicked = clickArray.every(function(item){
+    var allClicked = clickArray.every(function(item) {
       return item;
     });
-    console.log(allClicked);
+    //console.log(allClicked);
     if (allClicked && bright < 255) {
-      bright += 5;
+      bright += 1.5;
     }
 
   }
@@ -113,7 +145,11 @@ function lightClicked() {
   var getText = lightQList[this.id].value();
 
   if (getText != '') {
-    this.class('lightButton');
+    //this.style('background', '#f2bf27');
+
+    //this.class('lightButton');
+
+    lightBList2[this.id].class('lightButton');
     this.clicked = true;
     textOut = getText.split(/\W+/);
     answers = answers.concat(textOut);
@@ -122,7 +158,7 @@ function lightClicked() {
         textOut.splice(i, 1);
       }
     }
-    lightPos.set(this.x, this.y);
+    lightPos.set(this.x + areaSize / 16, this.y + areaSize / 12);
     lightOn = true;
   }
 }
@@ -135,18 +171,19 @@ function Light(posX, posY, word) {
   this.fontS = 0.5;
   this.photon = createButton(word);
   this.vel = 2;
-
+  this.opacity = 1;
 
   this.update = function() {
     this.size = map(this.s, 0, 90, 10, 80);
     this.fontS = map(this.s, 0, 90, 0.5, 4);
+    this.opacity = map(this.s, 0, 60, 1, 0.4);
     this.pos.y += this.vel;
     this.s++;
   };
 
   this.display = function() {
     this.photon.size(5 * this.size, this.size);
-    this.photon.style('color', 'orange');
+    this.photon.style('color', 'rgba(255,191,0,' + this.opacity + ')');
     this.photon.style('font-size', this.fontS + 'em');
     this.photon.style('font-family', 'monospace');
     this.photon.style('outline', 'none');
