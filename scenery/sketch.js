df30448591fe8caf1758a140d4f20df78f4f08c3
yumengwang03 <wangy594@newschool.edu>
2016-04-27 01:39:35 -0400

    😥

--- scenery/sketch.js
+++ scenery/sketch.js
@@ -1,7 +1,33 @@
+var sceneryRunOnce;
+var poemStarted;
+
 function setup() {
-  
+  sceneryRunOnce = 0;
+  poemStarted = false;
 }
 
 function draw() {
+  if (sceneryRunOnce < 1) {
   
+  sceneryRunOnce++;
+  poemStarted = true;
+  }
+  
+  if (poemStarted) {
+    
+  }
+}
+
+function Thing(word, xPos, yPos, mode, img) {
+  this.poemPos = createVector(0, 0);
+  this.poemPos.set(xPos, yPos);
+  this.word = word;
+  
+  this.display = function() {
+    
+  }
+  
+  this.update = function() {
+    
+  }
 }
