5880275bc9d0a8b41643f882c0d39116e18cdecf
yumengwang03 <wangy594@newschool.edu>
2016-05-08 04:00:52 -0400

    more cfg

--- code/rita-grammar.js
+++ code/rita-grammar.js
@@ -3,15 +3,22 @@ var corpora = require('corpora-project');
 // basics
 var nouns = corpora.getFile('words', 'nouns').nouns;
 
-function getVerbs() {
+function getVerbs(tense) {
     var verbList = corpora.getFile('words', 'verbs').verbs;
-    var verbsPast = [];
-    for (var i = 0; i < verbList.length; i++) {
-        verbsPast.push(verbList[i].past);
+    var verb_tense_list = [];
+    if (tense == "past") {
+        for (var i = 0; i < verbList.length; i++) {
+            verb_tense_list.push(verbList[i].past);
+        }
+    } else if (tense == "ing") {
+        for (var i = 0; i < verbList.length; i++) {
+            verb_tense_list.push(verbList[i].present + "ing");
+        }
     }
-    return verbsPast;
+    return verb_tense_list;
 }
-var verbs = getVerbs();
+var verbs_past = getVerbs("past");
+var verbs_ing = getVerbs("ing");
 
 var adjs = corpora.getFile('words', 'adjs').adjs;
 var advs = corpora.getFile('words', 'adverbs').adverbs;
@@ -37,7 +44,7 @@ var rita = require('rita');
 // var apple = r.getPosTags(rs);
 // console.log(apple);
 
-// var APIKEY = 'a644d55cb27d36059d70e0deac5021b42eb12486bc0b530c8';
+// var APIKEY = '';
 // var Wordnik = require('wordnik-bb').init(APIKEY);
 // var word = new Wordnik.Word({word: 'king', params:{includeSuggestions:true}});
 // word.getEverything()
@@ -45,29 +52,21 @@ var rita = require('rita');
 //     console.log("A WHOLE lot of data in a Word model: ", word);
 //   });
 
-
-
-//console.log(grammarJSON);
-
 var cfg = {
-    "<start>": "<np> <vp>",
-    "<np>": "<det> <n> | <det> <adj> <n> | <det> <adv> <adj> <n>",
-    "<vp>": "<v> | <v> <np>[2] | <adv> <v> | <adv> <v> <np>",
-    "<det>": "a | the"
+    "<start>": "<np> <vp>. | <np> <vp>, <clause1>. | <clause1>, <np> <vp>.",
+    "<np>": "<det> <n>[3] | <det> <adj> <n>[2] | <det> <adv> <adj> <n>",
+    "<vp>": "<v> | <v> <np>[3] | <adv> <v> | <adv> <v> <np>[2]",
+    "<det>": "a | the[4]",
     // <v>
+    // <v_ing>
     // <n>
     // <adj>
     // <adv>
+    "<location>": "in | on | at | in front of | before | after | above | below | through",
+    "<clause1>": "<v_ing> <np> | <v_ing> <location> <np> | <v> <np> | <v> <location> <np>"
 }
 
-
 var grammar = rita.RiGrammar(cfg);
-
-// console.log(grammar["<start>"]);
-var testData = "Yumeng | Chuck | Fame | Umi";
-
-
-// console.log(nouns.nouns[10]);
 
 function getNonTerminals(pos) {
     var nonTerminals;
@@ -78,11 +77,16 @@ function getNonTerminals(pos) {
 }
 
 var data_noun = getNonTerminals(nouns);
-var data_verb = getNonTerminals(verbs);
-//console.log(data_verb);
+var data_verb = getNonTerminals(verbs_past);
+var data_verb_ing = getNonTerminals(verbs_ing);
+var data_adj = getNonTerminals(adjs);
+var data_adv = getNonTerminals(advs);
 
 grammar.addRule("<n>", data_noun, 1);
 grammar.addRule("<v>", data_verb, 1);
+grammar.addRule("<v_ing>", data_verb_ing, 0.5);
+grammar.addRule("<adj>", data_adj, 1);
+grammar.addRule("<adv>", data_adv, 1);
 
 //console.log(grammar.hasRule("<6>"));
 
